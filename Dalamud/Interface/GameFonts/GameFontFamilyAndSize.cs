a44a7d65e3d6ea901dfe3e8cf4eeb6ec4eec55f0
DoHwa <kangdohwa0310@gmail.com>
2024-04-20 13:42:34 +0900

    build: 9.1.0.7
    forgot that zero

--- Dalamud/Interface/GameFonts/GameFontFamilyAndSize.cs
+++ Dalamud/Interface/GameFonts/GameFontFamilyAndSize.cs
@@ -23,7 +23,7 @@ public enum GameFontFamilyAndSize
     ///
     /// Contains Japanese characters in addition to Latin characters. Used in game for the whole UI.
     /// </summary>
-    [GameFontFamilyAndSize("common/font/KrnAXIS_12.fdt", "common/font/font_krn_{0}.tex", -1)]
+    [GameFontFamilyAndSize("common/font/KrnAXIS_120.fdt", "common/font/font_krn_{0}.tex", -1)]
     Axis12,
 
     /// <summary>
@@ -31,7 +31,7 @@ public enum GameFontFamilyAndSize
     ///
     /// Contains Japanese characters in addition to Latin characters. Used in game for the whole UI.
     /// </summary>
-    [GameFontFamilyAndSize("common/font/KrnAXIS_14.fdt", "common/font/font_krn_{0}.tex", -1)]
+    [GameFontFamilyAndSize("common/font/KrnAXIS_140.fdt", "common/font/font_krn_{0}.tex", -1)]
     Axis14,
 
     /// <summary>
@@ -39,7 +39,7 @@ public enum GameFontFamilyAndSize
     ///
     /// Contains Japanese characters in addition to Latin characters. Used in game for the whole UI.
     /// </summary>
-    [GameFontFamilyAndSize("common/font/KrnAXIS_18.fdt", "common/font/font_krn_{0}.tex", -1)]
+    [GameFontFamilyAndSize("common/font/KrnAXIS_180.fdt", "common/font/font_krn_{0}.tex", -1)]
     Axis18,
 
     /// <summary>
@@ -47,7 +47,7 @@ public enum GameFontFamilyAndSize
     ///
     /// Contains Japanese characters in addition to Latin characters. Used in game for the whole UI.
     /// </summary>
-    [GameFontFamilyAndSize("common/font/KrnAXIS_36.fdt", "common/font/font_krn_{0}.tex", -4)]
+    [GameFontFamilyAndSize("common/font/KrnAXIS_360.fdt", "common/font/font_krn_{0}.tex", -4)]
     Axis36,
 
     /// <summary>
